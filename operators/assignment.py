20b8c0784f228a50de276572cccbec2c3d4e33fc
karlla souzza <karlla.souzza7@gmail.com>
2026-04-09 23:36:21 -0300

    feat(operators): add assignment operations examples
    - Initialize saldo with a value of 100
    - Demonstrate various assignment operations with saldo
    - Print saldo after each operation to show changes

---
+++ operators/assignment.py
@@ -0,0 +1,28 @@
+saldo = 100
+
+print(saldo)
+
+saldo = 200
+print(saldo)
+
+
+saldo += 50
+print(saldo)
+
+saldo -= 30
+print(saldo)
+
+saldo *= 2
+print(saldo)
+
+saldo /= 4
+print(saldo)
+
+saldo //= 3
+print(saldo)
+
+saldo **= 2
+print(saldo)
+
+saldo %= 5
+print(saldo)
