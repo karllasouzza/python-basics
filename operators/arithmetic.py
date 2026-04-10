1ab9ba2edac726b721619444113b5e3dea3d82c8
karlla souzza <karlla.souzza7@gmail.com>
2026-04-09 22:49:15 -0300

    feat(operadores-aritmeticos): add arithmetic operations examples
    - Introduced basic arithmetic operations using two products.
    - Included addition, subtraction, division, floor division, multiplication, exponentiation, and modulus.

---
+++ operators/arithmetic.py
@@ -0,0 +1,17 @@
+product_1 = 20
+product_2 = 10
+
+print(product_1 + product_2)
+print(product_1 + product_2 + 3.5)
+
+print(product_1 - product_2)
+
+print(product_1 / product_2)
+print(product_1 // product_2)
+
+print(product_1 * product_2)
+
+print(product_1 ** product_2)
+
+print(product_1 % product_2)
+
